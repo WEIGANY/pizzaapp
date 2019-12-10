c9ef38306abe3985e61715fa6f92f30066adeee1
yaokk <yaowg539@gmail.com>
2019-12-10 18:38:31 +0800

    fix format print bug

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -41,6 +41,38 @@ const Item = styled.li`
   grid-template-columns: 1fr auto;
 `;
 
+function number_format(number, decimals, dec_point, thousands_sep) {
+  /*
+  * 参数说明：
+  * number：要格式化的数字
+  * decimals：保留几位小数
+  * dec_point：小数点符号
+  * thousands_sep：千分位符号
+  * */
+  number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function (n, prec) {
+          var k = Math.pow(10, prec);
+          return '' + Math.ceil(n * k) / k;
+      };
+
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  var re = /(-?\d+)(\d{3})/;
+  while (re.test(s[0])) {
+      s[0] = s[0].replace(re, "$1" + sep + "$2");
+  }
+
+  if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
 const Summary = ({
   selectedPizza: { name, price },
   selectedToppings,
@@ -64,7 +96,7 @@ const Summary = ({
               <button type="button" onClick={() => onToppingAmountAdd({ name, price })}>+</button>
             </ItemAmount>
           </ItemName>
-          <span>${price * amount}</span>
+          <span>${number_format(price * amount,2,".",",")}</span>
         </Item>   
       ))}    
     </ItemsList>
@@ -73,7 +105,7 @@ const Summary = ({
       <span>Total:</span>
       <span data-testid="summary-total">
         ${selectedToppings.reduce((previousValue, currentValue) => {
-          return previousValue + currentValue.price * currentValue.amount;
+          return number_format(previousValue + currentValue.price * currentValue.amount,2,".",",");
         }, price)}
       </span>
     </Item>
